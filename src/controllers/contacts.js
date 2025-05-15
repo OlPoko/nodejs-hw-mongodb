9a42a790a6fb25784f1e03b4ceffa27c90ad8723
SPok <s.pokotilo@gmail.com>
2025-05-15 20:00:33 +0300

    add pagination

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,20 +6,21 @@ import {
   deleteContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
-export const getContactsController = async (req, res, next) => {
-  try {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  } catch (err) {
-    next(err);
-  }
+export const getContactsController = async (req, res) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
 };
-
 export const getContactByIdController = async (req, res, next) => {
   try {
     const { contactId } = req.params;
