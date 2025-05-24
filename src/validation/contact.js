6f7e0e082e20122f169ad5484b55ec49e11b4b07
SPok <s.pokotilo@gmail.com>
2025-05-24 10:26:59 +0300

    update shema

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -1,11 +1,17 @@
-// src/validation/contacts.js
-
 import Joi from 'joi';
 
-export const updateContactSchema = Joi.object({
-  name: Joi.string().trim().min(3).max(20).required(),
+export const createContactSchema = Joi.object({
+  name: Joi.string().trim().required(),
   phoneNumber: Joi.string().trim().required(),
-  email: Joi.string().trim().email().optional(),
+  email: Joi.string().email().trim().optional(),
   isFavourite: Joi.boolean().optional(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().trim().optional(),
+  phoneNumber: Joi.string().trim().optional(),
+  email: Joi.string().email().trim().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
+}).min(1);
